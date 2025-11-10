05bcdbba39b8e2d4e0608687c3a941cbfbde70e1
joshianirudh <joshianirudh26@gmail.com>
2025-11-09 23:57:14 -0500

    update fixes

--- pytorch-training/train_classify.py
+++ pytorch-training/train_classify.py
@@ -8,7 +8,7 @@ class Iris(Dataset):
 
     def __init__(self):
         iris = load_iris()
-        X, y = iris.data, iris.targets
+        X, y = iris.data, iris.target
         self.features = torch.tensor(X, dtype=torch.float32)
         self.labels = torch.tensor(y, dtype=torch.long)
 
@@ -26,45 +26,47 @@ class Model(nn.Module):
     def __init__(self, input_shape, output_shape):
         super().__init__()
         self.l1 = nn.Linear(input_shape, 128)
+        self.drop1 = nn.Dropout(0.25)
         self.l2 = nn.Linear(128, 256)
+        self.drop2 = nn.Dropout(0.25)
         self.l3 = nn.Linear(256, 512)
+        self.drop3 = nn.Dropout(0.25)
         self.l4 = nn.Linear(512, 256)
+        self.drop4 = nn.Dropout(0.25)
         self.l5 = nn.Linear(256, output_shape)
 
     def forward(self, x):
 
-        x = F.relu(self.l1(x))
-        x = F.relu(self.l2(x))
-        x = F.relu(self.l3(x))
-        x = F.relu(self.l4(x))
+        x = F.relu(self.drop1(self.l1(x)))
+        x = F.relu(self.drop2(self.l2(x)))
+        x = F.relu(self.drop3(self.l3(x)))
+        x = F.relu(self.drop4(self.l4(x)))
         x = self.l5(x)
         return x
 
 def train(model, train_loader):
     optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
     loss_fn = torch.nn.CrossEntropyLoss()
-    EPOCHS = 10
+    EPOCHS = 50
     for epoch in range(EPOCHS):
         print(f"epoch {epoch + 1}")
         # enable train mode
-        running_loss = 0
         model.train(True)
+        running_loss, correct, total = 0.0, 0, 0
         for i, data in enumerate(train_loader):
             inputs, labels = data
-            # make sure grads are zeroed
-            optimizer.zero_grad()
-            #predict with model
             outputs = model(inputs)
-            # calculate the loss
             loss = loss_fn(outputs, labels)
             loss.backward()
             optimizer.step()
             outputs = outputs.argmax(dim=1)
-            acc = (outputs == labels).float().mean()
-            # adjust the weights
+            correct = (outputs == labels).float().sum()
+            total = inputs.size(0)
             running_loss += loss.item() 
+
+            optimizer.zero_grad()
         
-        print(f"Loss {running_loss}, Acc {acc}")
+        print(f"Loss {running_loss}, Acc {correct/total}")
 
 
 def evaluate(model, test_loader):
@@ -72,22 +74,18 @@ def evaluate(model, test_loader):
     model.eval()
     loss_fn = torch.nn.CrossEntropyLoss()
     with torch.no_grad():
-        loss , acc, total = 0, 0, 0
+        loss , correct, total = 0.0, 0, 0
 
         for i, data in enumerate(test_loader):
             inputs, labels = data
             logits = model(inputs)
             loss += loss_fn(logits, labels).item()
             logits = logits.argmax(dim=1)
-            acc += (logits == labels).float().sum()
+            correct += (logits == labels).float().sum()
             total += inputs.size(0)
     
 
-        print(f"Eval Loss: {loss}, Eval acc: {acc/total}")
-
-            
-
-
+        print(f"Eval Loss: {loss}, Eval acc: {correct/total}")
 
 
 if __name__ == "__main__":
